337f90438d1ec2fc8f3f1c23b5ad86f9ebeb1bf3
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-21 10:04:13 +0700

    Menambahkan dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
