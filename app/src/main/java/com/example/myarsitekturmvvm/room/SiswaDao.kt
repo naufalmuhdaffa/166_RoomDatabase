9d0598fdf5359e13b21c6ce2cbba00875544ed77
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-12-05 09:42:41 +0700

    menambahkan 3 fungsi pada Data Access Object

--- app/src/main/java/com/example/myarsitekturmvvm/room/SiswaDao.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/room/SiswaDao.kt
@@ -13,4 +13,13 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
+    @Update
+    suspend fun update(siswa: Siswa)
 }
