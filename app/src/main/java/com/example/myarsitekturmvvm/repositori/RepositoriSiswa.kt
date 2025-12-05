11fd61ed7d1a19784692747f68a3b3ad55972a92
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-12-05 09:56:45 +0700

    menambahkan fungsi

--- app/src/main/java/com/example/myarsitekturmvvm/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/repositori/RepositoriSiswa.kt
@@ -8,6 +8,10 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
 
     suspend fun insertSiswa(siswa: Siswa)
+
+    fun getAllSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -15,4 +19,8 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getAllSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
